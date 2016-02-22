d1f95b30213ede3dbb45f8661f4ceb7dcfc73856
Jo Young Jeong <balhae79@gmail.com>
2016-02-22 09:37:26 +0900

    Update EchoServer.java

--- src/main/java/net/joyoungc/server/netty/echo/EchoServer.java
+++ src/main/java/net/joyoungc/server/netty/echo/EchoServer.java
@@ -23,7 +23,7 @@ public final class EchoServer {
 			
 			ServerBootstrap b = new ServerBootstrap();
 			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class);
-//			.option(ChannelOption.SO_BACKLOG, 1000)
+			.option(ChannelOption.SO_BACKLOG, 100) // 서버가 동시에 100개의 연결 요청을 수용하도록 설정
 			b.handler(new LoggingHandler(LogLevel.INFO));
 			b.childHandler(new ChannelInitializer<SocketChannel>() {
 				@Override
